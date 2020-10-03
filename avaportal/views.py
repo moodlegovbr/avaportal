796ef4b49cdc6c867f3bd583542c45f9ce3f4093
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-10-03 15:52:21 -0300

    [ADD] home view

--- avaportal/views.py
+++ avaportal/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Campus
+
 
 # Create your views here.
+def index(request):
+    campus = Campus.objects.all()
+    return render(request, 'avaportal/index.html', {'campus': campus})
--- urls.py
+++ urls.py
@@ -3,33 +3,15 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from rest_framework import urls, schemas
-
-admin.site.site_url = '/' + settings.URL_PATH_PREFIX
+from avaportal.views import index
 
 urlpatterns = [
-    path(
-        settings.URL_PATH_PREFIX,
-        include(
-            [
-                path('admin/', admin.site.urls),
-                path('', include('django.contrib.auth.urls')),
-                path('', include('solicitacao.urls', namespace='solicitacao')),
-                path('api', schemas.get_schema_view(
-                    title="ID - Identidade do usuários",
-                    description="API for all things …",
-                    version="1.0.0"
-                ), name='openapi-schema'),    
-
-                # path('api-auth/', include('rest_framework.urls')),
-                # path('logout/', jwt_logout, name='logout'),
-                # path('', include('suap_ead.urls', namespace='suap_ead')),
-            ]
-        )
-    ),
-    path('', RedirectView.as_view(url=settings.URL_PATH_PREFIX)),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/login/', RedirectView.as_view(url='/oauth/login/suap/')),
+    path('admin/', admin.site.urls),
+    path('', index),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
-    urlpatterns.append(path('%s__debug__/' % settings.URL_PATH_PREFIX, include(debug_toolbar.urls)))
+    urlpatterns.append(path('/__debug__/', include(debug_toolbar.urls)))
