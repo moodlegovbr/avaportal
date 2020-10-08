7a990e5805626d95744152cefab8fde0ec59f71e
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-10-08 17:05:59 -0300

    [ADD] sync_up view

--- src/avaportal/views.py
+++ src/avaportal/views.py
@@ -1,5 +1,9 @@
-from django.shortcuts import render
-from .models import Campus
+import json
+from django.core.exceptions import PermissionDenied
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from .models import Campus, Solicitacao
 
 
 # Create your views here.
@@ -7,5 +11,38 @@ def index(request):
     campus = Campus.objects.all()
     return render(request, 'avaportal/index.html', {'campus': campus})
 
+
+def raise_error(request, error):
+    solicitacao = Solicitacao()
+    solicitacao.requisicao_header = request.META
+    solicitacao.requisicao_invalida = request.body.decode('utf-8')
+    solicitacao.resposta = error
+    solicitacao.status = Solicitacao.Status.FALHA
+    solicitacao.save()
+    response = JsonResponse(error)
+    response.status_code=error["code"]
+    return response
+
+@csrf_exempt
 def sync_up(request):
-    pass
+    if request.method != 'POST':
+        return HttpResponse("Mandou via GET pq?")
+
+    try:
+        pkg = json.loads(request.body)
+        filter = {"suap_id": pkg["campus"]["id"], "sigla": pkg["campus"]["sigla"]}
+    except Exception as e:
+        return raise_error(request, {"error": f"O JSON está inválido. {e}", "code": 406})
+    
+    campus = Campus.objects.filter(**filter).first()
+    if campus is None:
+        return raise_error(request, {"error": f"Não existe um campus com o id '{filter['suap_id']}' E a sigla '{filter['sigla']}'.", "code": 404})
+
+    if not campus.active:
+        return raise_error(request, {"error": f"O campus '{filter['sigla']}' existe, mas está inativo.", "code": 412})
+
+    solicitacao = Solicitacao()
+    solicitacao.requisicao_header = request.META
+
+    return JsonResponse(campus.active, safe=False)
+
--- src/urls.py
+++ src/urls.py
@@ -3,12 +3,13 @@ from django.contrib import admin
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
-from avaportal.views import index
+from avaportal.views import index, sync_up
 
 urlpatterns = [
     path('oauth/', include('social_django.urls', namespace='social')),
     path('admin/login/', RedirectView.as_view(url='/oauth/login/suap/')),
     path('admin/', admin.site.urls),
+    path('ava/barramento/api/moodle_suap/', sync_up), 
     path('', index),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + \
     static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
