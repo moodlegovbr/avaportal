5c7df6dcf8631c393a309685eb12a3c746435049
Kelson da Costa Medeiros <kelsoncm@gmail.com>
2020-11-05 19:21:57 -0300

    [FIX] If RP dont inform HTTP_X_FORWARDED_PROTO http will be assumed.

--- src/avaportal/middleware.py
+++ src/avaportal/middleware.py
@@ -16,9 +16,12 @@ class GoToHTTPSMiddleware(MiddlewareMixin):
     .. note::
         ``settings.GO_TO_HTTPS`` needs to be True.
         The RP need to inform HTTP_X_FORWARDED_PROTO and HTTP_X_FORWARDED_HOST.
+        If RP dont inform HTTP_X_FORWARDED_PROTO http will be assumed.
     """
     def process_request(self, request):
         if getattr(settings, 'GO_TO_HTTPS', False):
             meta = request.META
             if 'HTTP_X_FORWARDED_PROTO' in meta and meta['HTTP_X_FORWARDED_PROTO'] == 'http':
+                return HttpResponseRedirect("https://" + meta['HTTP_X_FORWARDED_HOST'] + meta['PATH_INFO'])
+            if 'HTTP_X_FORWARDED_PROTO' not in meta:
                 return HttpResponseRedirect("https://" + meta['HTTP_X_FORWARDED_HOST'] + meta['PATH_INFO'])
