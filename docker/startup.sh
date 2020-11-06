b17d6ed3b9bfe89989538f10da266834909a7d3c
root <root@alken.ifrn.local>
2020-11-05 21:21:37 -0300

    [DEL] gunicorn

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
-if [[ "True" = "$DJANGO_DEBUG" ]]; then
-    ./manage.sh runserver 0.0.0.0:8000
-else
-    gunicorn \
-        wsgi:application \
-        --bind 0.0.0.0:8000 \
-        --name root \
-        --user root \
-        --group root \
-        --timeout $GUNICORN_TIMEOUT \
-        --workers $GUNICORN_NUM_WORKERS \
-        --log-level $GUNICORN_LOG_LEVEL \
-        --forwarded-allow-ips "*" \
-        --log-file=-
-fi
+./manage.sh runserver 0.0.0.0:8000
+#if [[ "True" = "$DJANGO_DEBUG" ]]; then
+#    ./manage.sh runserver 0.0.0.0:8000
+#else
+#    gunicorn \
+#        wsgi:application \
+#        --bind 0.0.0.0:8000 \
+#        --name root \
+#        --user root \
+#        --group root \
+#        --timeout $GUNICORN_TIMEOUT \
+#        --workers $GUNICORN_NUM_WORKERS \
+#        --log-level $GUNICORN_LOG_LEVEL \
+#        --forwarded-allow-ips "*" \
+#        --log-file=-
+#fi
+
